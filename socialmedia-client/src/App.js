f5ba882abb5c80a6c1f426df44c534d5e0923fcd
fadeomar <fadeomar2015@gmail.com>
2020-01-28 12:57:52 +0200

    create redux store

--- socialmedia-client/src/App.js
+++ socialmedia-client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import jwtDecode from "jwt-decode";
+//Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 import Home from "./pages/home";
 import Signup from "./pages/signup";
 import Login from "./pages/login";
 import Navbar from "./components/navbar";
 import themeFile from "./utils/theme";
+import AuthRoute from "./utils/authRoute";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import "./App.css";
 
@@ -17,7 +21,7 @@ const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 < Date.now()) {
-    window.location.href = "/login";
+    // window.location.href = "/login";
     authenticated = false;
   } else {
     authenticated = true;
@@ -27,18 +31,28 @@ if (token) {
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <div className="App">
+      <Provider store={store}>
         <Router>
           <Navbar />
           <div className="container">
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
+              <AuthRoute
+                exact
+                path="/login"
+                component={Login}
+                authenticated={authenticated}
+              />
+              <AuthRoute
+                exact
+                path="/signup"
+                component={Signup}
+                authenticated={authenticated}
+              />
             </Switch>
           </div>
         </Router>
-      </div>
+      </Provider>
     </MuiThemeProvider>
   );
 }
