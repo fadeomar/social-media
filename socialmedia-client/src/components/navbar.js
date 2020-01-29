0deec27e3c655f6d677902d531c1391ccfcb5594
fadeomar <fadeomar2015@gmail.com>
2020-01-29 18:26:49 +0200

    updata the nav if the user authenticated

--- socialmedia-client/src/components/navbar.js
+++ socialmedia-client/src/components/navbar.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 
+import AddIcon from "@material-ui/icons/Add";
+import HomeIcon from "@material-ui/icons/Home";
+import Notifications from "@material-ui/icons/Notifications";
+
 import { connect } from "react-redux";
+
+import CustomButton from "../utils/CustomButton";
 
 class Navbar extends Component {
   render() {
@@ -15,7 +21,19 @@ class Navbar extends Component {
       <AppBar>
         <Toolbar className="nav-container">
           {authenticated ? (
-            <></>
+            <>
+              <CustomButton tip="Post a scream">
+                <AddIcon color="primary" />
+              </CustomButton>
+              <Link to="/">
+                <CustomButton tip="Home">
+                  <HomeIcon color="primary" />
+                </CustomButton>
+              </Link>
+              <CustomButton tip="notifiactions">
+                <Notifications color="primary" />
+              </CustomButton>
+            </>
           ) : (
             <>
               <Button color="inherit" component={Link} to="/login">
@@ -39,8 +57,8 @@ const mapStateToProps = state => ({
   authenticated: state.user.authenticated
 });
 
-Navbar.proptypes = {
-  authenticated: PropTypes.object.isRequired
-};
+// Navbar.proptypes = {
+//   authenticated: PropTypes.object.isRequired
+// };
 
 export default connect(mapStateToProps)(Navbar);
