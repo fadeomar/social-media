8216737cf48cf171b0f786a8b675f8b0f2196460
fadeomar <fadeomar2015@gmail.com>
2020-01-29 17:55:28 +0200

    create dialog for edit the user datils

--- socialmedia-client/src/components/editDetails.js
+++ socialmedia-client/src/components/editDetails.js
@@ -15,6 +15,8 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import EditIcon from "@material-ui/icons/Edit"; ///
+import IconButton from "@material-ui/core/IconButton";
 
 const styles = {};
 class EditDetails extends Component {
@@ -27,17 +29,105 @@ class EditDetails extends Component {
 
   componentDidMount() {
     const { credentials } = this.props;
+    this.mapUserDetailsToState(credentials);
+  }
+
+  mapUserDetailsToState = credentials => {
     this.setState({
       bio: credentials.bio ? credentials.bio : "",
       website: credentials.website ? credentials.website : "",
       location: credentials.location ? credentials.location : ""
     });
-  }
+  };
+
+  handleOpen = () => {
+    this.setState({
+      open: true
+    });
+    this.mapUserDetailsToState(this.props.credentials);
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = () => {
+    const userDetails = {
+      bio: this.state.bio,
+      website: this.state.website,
+      location: this.state.location
+    };
+    this.props.editUserDetails(userDetails);
+    this.handleClose();
+  };
 
   render() {
+    const { classes } = this.props;
     return (
       <>
-        <Tooltip title="Edit Details" placement="top"></Tooltip>
+        <Tooltip title="Edit Details" placement="top">
+          <IconButton onClick={this.handleOpen} className={classes.button}>
+            <EditIcon color="primary" />
+          </IconButton>
+        </Tooltip>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth
+          maxWidth="sm"
+        >
+          <DialogTitle>Edit your Details</DialogTitle>
+          <DialogContent>
+            <form>
+              <TextField
+                name="bio"
+                type="text"
+                label="Bio"
+                multiline
+                rows="3"
+                placeholder="A short bio about yourself"
+                className={classes.textField}
+                value={this.state.bio}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <TextField
+                name="location"
+                type="text"
+                label="Location"
+                placeholder=" Where you live"
+                className={classes.textField}
+                value={this.state.location}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <TextField
+                name="website"
+                type="text"
+                label="website"
+                placeholder="Your personal/profissional website "
+                className={classes.textField}
+                value={this.state.website}
+                onChange={this.handleChange}
+                fullWidth
+              />
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancle
+            </Button>
+            <Button onClick={this.handleSubmit} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
       </>
     );
   }
