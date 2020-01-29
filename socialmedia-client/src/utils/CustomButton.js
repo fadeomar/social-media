294e09ee65498ab157535fda6ee3ba9cb10c4934
fadeomar <fadeomar2015@gmail.com>
2020-01-29 19:12:11 +0200

    create our cusyom btn

--- socialmedia-client/src/utils/CustomButton.js
+++ socialmedia-client/src/utils/CustomButton.js
@@ -2,10 +2,10 @@ import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
 
-export default ({ childern, onClick, tip, btnClass, tipClass }) => (
+export default ({ children, onClick, tip, btnClass, tipClass }) => (
   <Tooltip title={tip} className={tipClass}>
     <IconButton onClick={onClick} className={btnClass}>
-      {childern}
+      {children}
     </IconButton>
   </Tooltip>
 );
