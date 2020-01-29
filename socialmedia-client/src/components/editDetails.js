d3314fe459e10839fb706bffd47afe070accf261
fadeomar <fadeomar2015@gmail.com>
2020-01-29 17:35:07 +0200

    connect the details comp to redux and and wet the local stae win component mount

--- socialmedia-client/src/components/editDetails.js
+++ socialmedia-client/src/components/editDetails.js
@@ -4,12 +4,54 @@ import withStyle from "@material-ui/core/styles/withStyles";
 import { style } from "@material-ui/system";
 
 import { connect } from "react-redux";
+import { editUserDetails } from "../redux/actions/userActions";
+
+//MUI stuff
+import Tooltip from "@material-ui/core/Tooltip";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 const styles = {};
-class editDetails extends Component {
+class EditDetails extends Component {
+  state = {
+    bio: "",
+    website: "",
+    location: "",
+    open: false
+  };
+
+  componentDidMount() {
+    const { credentials } = this.props;
+    this.setState({
+      bio: credentials.bio ? credentials.bio : "",
+      website: credentials.website ? credentials.website : "",
+      location: credentials.location ? credentials.location : ""
+    });
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <>
+        <Tooltip title="Edit Details" placement="top"></Tooltip>
+      </>
+    );
   }
 }
 
-export default withStyle(editDetails, styles);
+const mapStateToProps = state => ({
+  credentials: state.user.credentials
+});
+
+EditDetails.prototype = {
+  editUserDetails: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps, { editUserDetails })(
+  withStyle(EditDetails, styles)
+);
