9fc3099cbacc515956fe0a71eb2db5f6dffcb8c3
fadeomar <fadeomar2015@gmail.com>
2020-01-29 12:00:57 +0200

    using loading user action in get data

--- socialmedia-client/src/redux/actions/userActions.js
+++ socialmedia-client/src/redux/actions/userActions.js
@@ -25,6 +25,7 @@ export const loginUser = (userData, history) => dispatch => {
 };
 
 export const getUserData = () => dispatch => {
+  dispatch({ type: LOADING_USER });
   axios
     .get("/user")
     .then(res => {
