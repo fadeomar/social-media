471625578cce8d846803cf21791216c579796caf
fadeomar <fadeomar2015@gmail.com>
2020-01-29 17:21:02 +0200

    create editDetails component

---
+++ socialmedia-client/src/components/editDetails.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import withStyle from "@material-ui/core/styles/withStyles";
+import { style } from "@material-ui/system";
+
+import { connect } from "react-redux";
+
+const styles = {};
+class editDetails extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default withStyle(editDetails, styles);
